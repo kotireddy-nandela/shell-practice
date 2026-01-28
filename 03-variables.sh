88592e6b6d4e00a480833923eaa4356a3f1833d7
dawsvenkat <kotireddyn2688@gmail.com>
2026-01-28 18:57:49 +0530

    Variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -8,4 +8,4 @@ echo "$PERSON2:: I am fine thank you, HOw about you?"
 echo "$PERSON1:: I am good, What are you learing"
 echo "$PERSON2:: I am learing DEVOPS with AWS and Shell"
 echo "$PERSON1:: Oh super  i am aslo learing  Shell"
-echo "$PERSON2:: excelent, Good topic
+echo "$PERSON2:: excelent, Good topic"
