79a5db51ff149bfe6014f6691cfbaac4a2e556c5
dawsvenkat <kotireddyn2688@gmail.com>
2026-01-29 18:20:27 +0530

    Conditions

--- 11-installation.sh
+++ 11-installation.sh
@@ -10,6 +10,14 @@ if [ $USERID -ne 0 ]; then
 fi
 
 echo "Installing Nginx"
-
-
 dnf install nginx -y
+
+if [ $? ne 0 ]; then
+  echo "Installing Nginx ....FAILURE"
+  exit 1
+
+else
+   echo "Installing Nginx ...Sucess"
+
+
+fi
